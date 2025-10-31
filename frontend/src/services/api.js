0ca8cbc6f72af9c79fb4ab0bf3784e88462ab8f8
Punit Tak <147526916+PunitTak2005@users.noreply.github.com>
2025-10-31 17:46:12 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 // Simple fetch wrapper
 export async function apiFetch(endpoint, options = {}) {
-  const baseUrl = "http://localhost:5000/api"; // Change as needed
+  const baseUrl = "https://restaurant-qr-menu-g5s5.onrender.com";
   const response = await fetch(`${baseUrl}${endpoint}`, options);
   if (!response.ok) throw new Error("API Error");
   return response.json();
